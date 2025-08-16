7eb6cc937ee21edacf2fdc45fe22d69442c9dad4
=Javier Lim <=s10241539@connect.np.edu.sg>
2025-08-16 15:45:32 +0000

    final deployment

--- delete/delete.php
+++ delete/delete.php
@@ -3,6 +3,6 @@ include 'db.php';
 
 $id = $_GET['id'];
 $conn->query("DELETE FROM users WHERE id=$id");
-header("https://ljpnho26p3.execute-api.us-east-1.amazonaws.com");
+header("https://ljpnho26p3.execute-api.us-east-1.amazonaws.com/");
 exit();
 ?>
